9934099871deb4afa14103c41f0dfe3b32083549
Alfarabi <a_agadilkhan@kbtu.kz>
2023-02-22 02:41:38 +0600

    finished cart, shipping

--- lab41/src/app/product-details/product-details.component.ts
+++ lab41/src/app/product-details/product-details.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+
 import { Product, products } from '../products';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -10,7 +12,13 @@ import { Product, products } from '../products';
 export class ProductDetailsComponent {
   product: Product | undefined;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(rivate route: ActivatedRoute,
+    private cartService: CartService) {}
+  
+  addToCart(product: Product) {
+    this.cartService.addToCart(product);
+    window.alert('Your product has been added to cart!');
+  }
 
 }
 ngOnInit() {
