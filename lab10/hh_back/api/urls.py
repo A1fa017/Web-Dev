48273d154179d4cb8d60ce6bca011a1fa6939a65
Alfarabi <a_agadilkhan@kbtu.kz>
2023-04-19 22:19:33 +0600

    finished backend side

--- lab10/hh_back/api/urls.py
+++ lab10/hh_back/api/urls.py
@@ -2,10 +2,14 @@ from django.urls import path
 from api import views
 
 urlpatterns = [
-    path('companies/', views.company_list),
-    path('companies/<int:company_id>/', views.company_detail),
+    # path('companies/', views.company_list),
+    # path('companies/<int:company_id>/', views.company_detail),
     # path('companies/<int:company_id>/vacancies/', views.company_vacancies),
-    path('vacancies/', views.vacancy_list),
-    path('vacancies/<int:vacancy_id>/', views.vacancy_detail),
+    # path('vacancies/', views.vacancy_list),
+    # path('vacancies/<int:vacancy_id>/', views.vacancy_detail),
     # path('vacancies/top_ten/', views.top_ten),
+    path('companies/', views.CompanyListAPIView.as_view()),
+    path('companies/<int:company_id>/', views.CompanyDetailAPIView.as_view()),
+    path('vacancies/', views.VacancyListAPIView.as_view()),
+    path('vacancies/<int:vacancy_id>/', views.VacancyDetailAPIView.as_view()),
 ]
--- lab10/hh_back/api/views/__init__.py
+++ lab10/hh_back/api/views/__init__.py
@@ -1,2 +1,2 @@
-# from .views import *
-from .fbv import company_list, company_detail, vacancy_list, vacancy_detail
+# from .fbv import company_list, company_detail, vacancy_list, vacancy_detail
+from .cbv import CompanyListAPIView, CompanyDetailAPIView, VacancyListAPIView, VacancyDetailAPIView
